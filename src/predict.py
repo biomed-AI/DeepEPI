47ccf080d07cfd5eb2cdb37da65966e8ffa407d7
chenken <chenkenck@outlook.com>
2021-01-29 10:44:24 +0800

    fix cpu

--- src/predict.py
+++ src/predict.py
@@ -56,15 +56,15 @@ def predict(model, test_data, device, sample_idxs='all', batch_size=128, evaluat
     use_local, use_global = False, True
 
     if type(sample_idxs) is str and sample_idxs == 'all':
-        sample_idxs = list(range(test_data['label'].shape[0]))
+        sample_idxs = list(range(test_data['chrom'].shape[0]))
     total_size = len(sample_idxs)
     n_batches = np.ceil(total_size / batch_size).astype(int)
     with torch.no_grad():
-        test_prob, test_true = None, None
+        test_prob = None
         for i in range(n_batches):
             batch_sample_idxs = sample_idxs[i * batch_size : (i + 1) * batch_size]
             # label = test_data['label'][batch_sample_idxs]
-            label = Tensor(label).to(device)
+            # label = Tensor(label).to(device)
 
             if model.use_dist:
                 dist = validate_data['dist'][batch_sample_idxs]
@@ -84,11 +84,11 @@ def predict(model, test_data, device, sample_idxs='all', batch_size=128, evaluat
 
             if test_prob is None:
                 test_prob = prob
-                test_true = label
+                # test_true = label
             else:
                 test_prob = torch.cat((test_prob, prob), dim=0)
-                test_true = torch.cat((test_true, label), dim=0)
-        loss = criterion(test_prob, test_true).item()
+                # test_true = torch.cat((test_true, label), dim=0)
+        # loss = criterion(test_prob, test_true).item()
     return test_prob.detach().cpu().numpy().squeeze()
 
 
@@ -97,9 +97,9 @@ def get_args():
     p.add_argument('-t', "--data_dirs", required=True, nargs='+')
     p.add_argument('-m', "--model-state", required=True, help="Use model state dict")
     p.add_argument('-c', "--model-config", required=True, help="Model parameters")
-    p.add_argument('-gf', "--global-features", default=["segment.mark_3", "segment.ep.pos-encode", "segment.CTCF.arcsinh", "segment.DNase-pval.arcsinh", "segment.H3K27ac.pval.arcsinh", "segment.H3K4me1.pval.arcsinh", "segment.H3K4me3.pval.arcsinh"], nargs='+', help="DeepEPI features")
+    p.add_argument('-gf', "--global-features", default=["segment.mark_3", "segment.ep.pos-encode", "segment.CTCF.arcsinh", "segment.DNase-pval.arcsinh", "segment.H3K27ac-pval.arcsinh", "segment.H3K4me1-pval.arcsinh", "segment.H3K4me3-pval.arcsinh"], nargs='+', help="DeepEPI features")
     p.add_argument('-b', "--batch-size", default=128, type=int, help="batch size")
-    p.add_argument('-o', '--output', help="Write prediction")
+    p.add_argument('-o', '--output', help="Write prediction", required=True)
     p.add_argument("--seed", type=int, default=2020)
     return p.parse_args()
 
@@ -124,8 +124,8 @@ if __name__ == "__main__":
     validate_data = prepare_data.load_multiple_datasets(args.data_dirs, global_features=args.global_features)
     print("- Test data size: {}".format("segment {}".format(validate_data['segment'].shape) if use_global else ""))
 
-    gpu_id = idle_gpu(min_memory=3072)
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+    # gpu_id = idle_gpu(min_memory=3072)
+    # os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print("- Using device: ", device)
 
@@ -140,10 +140,15 @@ if __name__ == "__main__":
     model.eval()
 
     print("- Loading model state from {}".format(args.model_state))
-    state_dict = torch.load(args.model_state)
+    if torch.cuda.is_available():
+        state_dict = torch.load(args.model_state)
+    else:
+        state_dict = torch.load(args.model_state, map_location=torch.device('cpu'))
     model.load_state_dict(state_dict['model_state_dict'])
 
     print("- Model\n", model, '\n', model_summary(model))
 
     results = predict(model, validate_data, device, sample_idxs='all', batch_size=args.batch_size, evaluation=True)
     print(results)
+    np.savetxt("{}/results.txt".format(args.output), results, fmt="%.5f")
+
